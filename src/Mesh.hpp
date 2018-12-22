4a77e38e9fa8fdc94fd37364703ea27691c7372c
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-12-22 22:09:39 +0000

    += operator overload

--- src/Mesh.hpp
+++ src/Mesh.hpp
@@ -10,6 +10,16 @@ struct Vertex {
     std::vector<GLfloat> colors;
     std::vector<GLfloat> texCoords;
     std::vector<GLuint> indices;
+
+    Vertex& operator += (const Vertex& a) {
+            this->vertices.insert(this->vertices.end(), std::make_move_iterator(a.vertices.begin()), std::make_move_iterator(a.vertices.end()));
+            this->normals.insert(this->normals.end(), std::make_move_iterator(a.normals.begin()), std::make_move_iterator(a.normals.end()));
+            this->colors.insert(this->colors.end(), std::make_move_iterator(a.colors.begin()), std::make_move_iterator(a.colors.end()));
+            this->texCoords.insert(this->texCoords.end(), std::make_move_iterator(a.texCoords.begin()), std::make_move_iterator(a.texCoords.end()));
+            this->indices.insert(this->indices.end(), std::make_move_iterator(a.indices.begin()), std::make_move_iterator(a.indices.end()));
+
+            return *this;
+    }
 };
 
 class Mesh {
@@ -24,12 +34,12 @@ private:
     void addVBO(int size, std::vector<GLfloat> data);
 
 public:
+    Mesh() = default;
     Mesh(Vertex vertexInfo);
     ~Mesh();
 
 public:
     void draw(GLenum);
 };
-
 
 #endif //CPP_MINECRAFT_MESH_H
