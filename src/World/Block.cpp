8f9c338ca9df9bcee29ebaabf7edfc7137f1d1ba
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2018-12-25 19:23:09 +0000

    Single function to generate all faces depending on the information provided by the greedy mesh algorithm.

--- src/World/Block.cpp
+++ src/World/Block.cpp
@@ -1,77 +1,83 @@
 #include "Block.hpp"
 
-Block::Block() : _active(true) {
+Block::Block() : _visable(true), _blockType(BlockType::Grass) {
 
 }
 
-bool Block::isActive() {
-    return _active;
+const bool Block::isVisable() {
+    return _visable;
 }
 
-void Block::setActive(bool state) {
-    _active = state;
+void Block::setVisable(bool state) {
+    _visable = state;
 }
 
-Vertex Block::CreateCube(int x, int y, int z, unsigned int vertexCount) {
-    float m = 0.25f;
+BlockType Block::getType() {
+    return _blockType;
+}
 
-    return Vertex {
+void Block::setType(BlockType type) {
+    _blockType = type;
+}
+
+Vertex Block::buildFace(GLfloat v0[3], GLfloat v1[3], GLfloat v2[3], GLfloat v3[3], int width, int height, face side, glm::ivec3 chunkPos, unsigned int vertexCount, BlockType type) {
+    float uM = 0.24f;
+    float vM = 0.24f;
+
+    glm::ivec3 offset {chunkPos * CHUNK_SIZE};
+
+    std::vector<GLfloat> texPos {};
+
+   if (side == face::LEFT || side == face::RIGHT)
+       texPos = {
+           0 , 1 + (GLfloat)height, (GLfloat)type,
+           1 + (GLfloat)width, 1 + (GLfloat)height, (GLfloat)type,
+           1 + (GLfloat)width, 0, (GLfloat)type,
+           0, 0, (GLfloat)type,
+       };
+
+   if (side == face::FRONT || side == face::BACK)
+       texPos = {
+           1 + (GLfloat)height, 1 + (GLfloat)height, (GLfloat)type,
+           1 + (GLfloat)height, 0, (GLfloat)type,
+           0, 0, (GLfloat)type,
+           0, 1 + (GLfloat)height, (GLfloat)type,
+       };
+   if (side == face::BOTTOM || side == face::TOP)
+       texPos = {
+           1 + (GLfloat)height, 0, (GLfloat)type,
+           1 + (GLfloat)height, 1 + (GLfloat)height, (GLfloat)type,
+           0, 1 + (GLfloat)height, (GLfloat)type,
+           0, 0, (GLfloat)type,
+       };
+
+    return Vertex{
             {
-                    x + 1.0f, y + 0.0f, z + 0.0f,     // Front Top Left
-                    x + 0.0f, y + 0.0f, z + 0.0f,     // Front Top Right
-                    x + 0.0f, y + 1.0f, z + 0.0f,     // Front Bottom Right
-                    x + 1.0f, y + 1.0f, z + 0.0f,     // Front Bottom Left
-                    x + 1.0f, y + 0.0f, z + 1.0f,     // Back Top Left
-                    x + 0.0f, y + 0.0f, z + 1.0f,     // Back Top Right
-                    x + 0.0f, y + 1.0f, z + 1.0f,     // Back Bottom Right
-                    x + 1.0f, y + 1.0f, z + 1.0f
+                v0[0] + offset.x, v0[1] + offset.y, v0[2] + offset.z,
+                v3[0] + offset.x, v3[1] + offset.y, v3[2] + offset.z,
+                v2[0] + offset.x, v2[1] + offset.y, v2[2] + offset.z,
+                v1[0] + offset.x, v1[1] + offset.y, v1[2] + offset.z,
             },
 
             {
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
+                0, 0, 0,
+                0, 0, 0,
+                0, 0, 0,
+                0, 0, 0,
             },
 
             {
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
-                    0, 0, 0,
+                0, 0, 0,
+                0, 0, 0,
+                0, 0, 0,
+                0, 0, 0,
             },
 
-            {       0, 1,
-                    m, 1,
-                    m, 1 - m,
-                    0, 1 - m,
-                    0, 1,
-                    m, 1,
-                    m, 1 - m,
-                    0, 1 - m,
-            },
+            texPos,
 
             {
-                    vertexCount + 0, vertexCount + 3, vertexCount + 2,  //Front
-                    vertexCount + 2, vertexCount + 1, vertexCount + 0,
-                    vertexCount + 1, vertexCount + 5, vertexCount + 6,	//Right
-                    vertexCount + 6, vertexCount + 2, vertexCount + 1,
-                    vertexCount + 5, vertexCount + 4, vertexCount + 7,	//Left
-                    vertexCount + 7, vertexCount + 6, vertexCount + 5,
-                    vertexCount + 4, vertexCount + 7, vertexCount + 3,	//Back
-                    vertexCount + 3, vertexCount + 0, vertexCount + 4,
-                    vertexCount + 4, vertexCount + 5, vertexCount + 1,	//Top
-                    vertexCount + 1, vertexCount + 0, vertexCount + 4,
-                    vertexCount + 3, vertexCount + 2, vertexCount + 6,	//Bottom
-                    vertexCount + 6, vertexCount + 7, vertexCount + 3
+                vertexCount + 0, vertexCount + 1,  vertexCount + 2,
+                vertexCount + 2, vertexCount + 3,  vertexCount + 0,
             },
     };
 }
--- src/World/Block.hpp
+++ src/World/Block.hpp
@@ -4,19 +4,32 @@
 #include "../includes.hpp"
 #include "../Mesh.hpp"
 
+enum face {
+    FRONT,
+    BACK,
+    RIGHT,
+    LEFT,
+    TOP,
+    BOTTOM,
+};
+
 class Block {
 private:
-    bool _active;
-    BlockType blockType;
+    bool _visable;
+    BlockType _blockType;
 
 public:
     Block();
 
 public:
-    bool isActive();
-    void setActive(bool state);
+    const bool isVisable();
+    void setVisable(bool state);
+    BlockType getType();
+    void setType(BlockType type);
 
-    static Vertex CreateCube(int x, int y, int z, unsigned int vertexCount);
+public:
+    static Vertex buildFace(GLfloat v0[3], GLfloat v1[3], GLfloat v2[3], GLfloat v3[3], int width, int height,
+            face side, glm::ivec3 chunkPos, unsigned int vertexCount, BlockType type);
 };
 
 
