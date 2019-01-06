feff5c501b49c0fc65462c0e23e4a4ed4ff0f1da
Domantas Lankutis <domantas.lankutis@ada.ac.uk>
2019-01-06 21:39:13 +0000

    Added a janky reticle.

--- src/Graphics/shaders/fShader.glsl
+++ src/Graphics/shaders/fShader.glsl
@@ -8,5 +8,9 @@ uniform sampler2DArray tex;
 
 void main()
 {
-    outColor = vec4(texture(tex, texCoord).rgb * ((Light / 30) + 0.05), 1);
+    if (gl_FragCoord.x >= 955 && gl_FragCoord.x <= 965 && gl_FragCoord.y >= 535 && gl_FragCoord.y <= 545) {
+        outColor = vec4(1, 1, 1, 1);
+    } else {
+        outColor = vec4(texture(tex, texCoord).rgb * ((Light / 30) + 0.05), 1);
+    }
 }
